f7fea0d4d31121b308b952f01a94681400e45ee9
khalidh sm <khalidhmhd@gmail.com>
2017-12-13 10:48:53 +0530

    Update 20171129140137_rename_columns_ectes_eamcets.rb

--- db/migrate/20171129140137_rename_columns_ectes_eamcets.rb
+++ db/migrate/20171129140137_rename_columns_ectes_eamcets.rb
@@ -1,7 +1,7 @@
 class RenameColumnsEctesEamcets < ActiveRecord::Migration[5.1]
   def change
-  	rename_column :ecets, :test_count, :testcount
-  	rename_column :ecets, :type, :imageyes
+  	#rename_column :ecets, :test_count, :testcount
+  	#rename_column :ecets, :type, :imageyes
   	rename_column :ecets, :que_image, :questionimage
   	
   	rename_column :eamcets, :test_count, :testcount
