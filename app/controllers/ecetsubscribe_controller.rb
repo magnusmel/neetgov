4ae7c3df5815d39f3f5ec88ed783f9eaadb661d8
entersoft <apps@entersoft.in>
2018-01-07 20:06:25 +0530

    subscribe forms added

--- app/controllers/ecetsubscribe_controller.rb
+++ app/controllers/ecetsubscribe_controller.rb
@@ -11,7 +11,7 @@ class EcetsubscribeController < ApplicationController
   	@apecet = Subscribe.new(subscribe_params)
   	if @apecet.valid?
   		@apecet.save!
-  		redirect_to "/#{subscribe_params['state']}-#{subscribe_params['state']}sbtet-results", :notice => "Thank you for subscribe."
+  		redirect_to "/#{subscribe_params['state']}-#{subscribe_params['state']}sbtet-results", :notice => "Thank you for subscribe, We will notify."
   	else
   		redirect_to "/#{subscribe_params['state']}-#{subscribe_params['state']}sbtet-results", :Alert => "Somthing went wrong, please try again"
   	end
